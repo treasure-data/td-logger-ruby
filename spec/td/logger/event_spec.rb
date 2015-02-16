34d8e63c256bb48ed2524a2b8b13b35f83c97550
uu59 <k@uu59.org>
2015-02-16 16:43:27 +0900

    Add spec for TreasureData::Logger::Event

--- spec/td/logger/event_spec.rb
+++ spec/td/logger/event_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe TreasureData::Logger::Event do
-  context 'preset' do
+  describe 'EventPreset' do
     let(:test_logger) do
       Fluent::Logger::TestLogger.new
     end
@@ -14,32 +14,122 @@ describe TreasureData::Logger::Event do
       TD.event.attribute.clear
     end
 
-    it 'action' do
-      expect(test_logger).to receive(:post).with(:doit, {:action=>"doit", :foo=>:bar, :uid=>"uid1"}).twice
-      TD.event.action(:doit, {:foo=>:bar}, "uid1")
-      TD.event.attribute[:uid] = "uid1"
-      TD.event.action(:doit, {:foo=>:bar})
+    describe 'action' do
+      context "`uid` filled" do
+        it do
+          expect(test_logger).to receive(:post).with(:doit, {:action=>"doit", :foo=>:bar, :uid=>"uid1"}).twice
+          TD.event.action(:doit, {:foo=>:bar}, "uid1")
+          TD.event.attribute[:uid] = "uid1"
+          TD.event.action(:doit, {:foo=>:bar})
+        end
+      end
+
+      context "`uid` unfilled" do
+        it do
+          expect { TD.event.action(:doit, {:foo=>:bar}, nil) }.to raise_error(ArgumentError)
+        end
+      end
     end
 
-    it 'register' do
-      expect(test_logger).to receive(:post).with(:register, {:action=>"register", :uid=>"uid1"}).twice
-      TD.event.register("uid1")
-      TD.event.attribute[:uid] = "uid1"
-      TD.event.register
+    describe 'register' do
+      context "`uid` filled" do
+        it do
+          expect(test_logger).to receive(:post).with(:register, {:action=>"register", :uid=>"uid1"}).twice
+          TD.event.register("uid1")
+          TD.event.attribute[:uid] = "uid1"
+          TD.event.register
+        end
+      end
+
+      context "`uid` unfilled" do
+        it do
+          expect { TD.event.register(nil) }.to raise_error(ArgumentError)
+        end
+      end
     end
 
-    it 'login' do
-      expect(test_logger).to receive(:post).with(:login, {:action=>"login", :uid=>"uid1"}).twice
-      TD.event.login("uid1")
-      TD.event.attribute[:uid] = "uid1"
-      TD.event.login
+    describe 'login' do
+      context "`uid` filled" do
+        it do
+          expect(test_logger).to receive(:post).with(:login, {:action=>"login", :uid=>"uid1"}).twice
+          TD.event.login("uid1")
+          TD.event.attribute[:uid] = "uid1"
+          TD.event.login
+        end
+      end
+
+      context "`uid` unfilled" do
+        it do
+          expect { TD.event.login(nil) }.to raise_error(ArgumentError)
+        end
+      end
     end
 
-    it 'pay' do
-      expect(test_logger).to receive(:post).with(:pay, {:action=>"pay", :category=>"cat", :sub_category=>"subcat", :name=>"name", :price=>1980, :count=>1, :uid=>"uid1"}).twice
-      TD.event.pay("cat", "subcat", "name", 1980, 1, "uid1")
-      TD.event.attribute[:uid] = "uid1"
-      TD.event.pay("cat", "subcat", "name", 1980, 1)
+    describe 'pay' do
+      context "`uid` filled" do
+        it do
+          expect(test_logger).to receive(:post).with(:pay, {:action=>"pay", :category=>"cat", :sub_category=>"subcat", :name=>"name", :price=>1980, :count=>1, :uid=>"uid1"}).twice
+          TD.event.pay("cat", "subcat", "name", 1980, 1, "uid1")
+          TD.event.attribute[:uid] = "uid1"
+          TD.event.pay("cat", "subcat", "name", 1980, 1)
+        end
+      end
+
+      context "`uid` unfilled" do
+        it do
+          expect { TD.event.pay("cat", "subcat", "name", 1980, 1, nil) }.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+
+  describe "Event" do
+    let(:attrs) { {default: "attr"} }
+    let(:event) { TreasureData::Logger::Event.new }
+
+    describe "#post" do
+      subject { event.post(action, record) }
+      let(:action) { "act" }
+      let(:record) { {hello: "hello"} }
+
+      context "with default attributes" do
+        before { event.attribute = attrs }
+
+        it "invoke TreasureData::Logger.post" do
+          expect(TreasureData::Logger).to receive(:post).with(action, attrs.merge(record))
+          subject
+        end
+      end
+
+      context "without default attributes" do
+        it "invoke TreasureData::Logger.post" do
+          expect(TreasureData::Logger).to receive(:post).with(action, record)
+          subject
+        end
+      end
+    end
+
+    describe "#post_with_time" do
+      subject { event.post_with_time(action, record, time) }
+      let(:action) { "act" }
+      let(:record) { {hello: "hello"} }
+      let(:time) { Time.now }
+
+      context "with default attributes" do
+        before { event.attribute = attrs }
+
+        it "invoke TreasureData::Logger.post_with_time" do
+          expect(TreasureData::Logger).to receive(:post_with_time).with(action, attrs.merge(record), time)
+          subject
+        end
+      end
+
+      context "without default attributes" do
+        it "invoke TreasureData::Logger.post_with_time" do
+          expect(TreasureData::Logger).to receive(:post_with_time).with(action, record, time)
+          subject
+        end
+      end
     end
   end
 end
