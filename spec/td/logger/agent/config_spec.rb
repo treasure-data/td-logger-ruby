6ee56e580719f44e5a8d734aaa530bc45b13b7e1
uu59 <k@uu59.org>
2015-02-16 17:11:16 +0900

    Add spec for TreasureData::Logger::Agent::Rails::Config

--- spec/td/logger/agent/config_spec.rb
+++ spec/td/logger/agent/config_spec.rb
@@ -80,6 +80,7 @@ EOF
       expect(c.database).to eq('db2')
       expect(c.auto_create_table).to eq(true)
       expect(c.debug_mode).to eq(true)
+      expect(c.access_log_enabled?).to eq false
     end
 
     it 'load_file without test' do
@@ -94,6 +95,7 @@ EOF
       }
       c = TreasureData::Logger::Agent::Rails::Config.init
       expect(c.disabled).to eq(true)
+      expect(c.access_log_enabled?).to eq false
     end
 
     it 'prefer file than env' do
@@ -116,6 +118,7 @@ EOF
       expect(c.database).to eq('db4')
       expect(c.auto_create_table).to eq(false)
       expect(c.debug_mode).to eq(false)
+      expect(c.access_log_enabled?).to eq false
     end
 
     it 'agent mode' do
@@ -133,6 +136,7 @@ EOF
       expect(c.tag).to eq('td.db5')
       expect(c.agent_host).to eq('localhost')
       expect(c.agent_port).to eq(24224)
+      expect(c.access_log_enabled?).to eq false
     end
 
     it 'test mode' do
@@ -148,6 +152,109 @@ EOF
       c = TreasureData::Logger::Agent::Rails::Config.init
       expect(c.disabled).to eq(false)
       expect(c.test_mode?).to eq(true)
+      expect(c.access_log_enabled?).to eq false
+    end
+
+    it "access_log_enabled" do
+      FileUtils.mkdir_p("#{TMP_DIR}/config")
+      File.open("#{TMP_DIR}/config/treasure_data.yml", "w") {|f|
+        f.write <<EOF
+test:
+  agent: localhost
+  tag: foo.bar
+  access_log_table: log
+EOF
+      }
+      c = TreasureData::Logger::Agent::Rails::Config.init
+      expect(c.disabled).to eq(false)
+      expect(c.access_log_enabled?).to eq true
+    end
+
+    describe "load_file_ey" do
+      context "config_path does not exists" do
+        it "load from CONFIG_PATH_EY_DEPLOY" do
+          FileUtils.mkdir_p("#{TMP_DIR}/config")
+          File.unlink("#{TMP_DIR}/config/treasure_data.yml") rescue nil
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_DEPLOY, "w") {|f|
+            f.write <<EOF
+td:
+  TREASURE_DATA_API_KEY: foo
+EOF
+          }
+          c = TreasureData::Logger::Agent::Rails::Config.init
+          expect(c.disabled).to eq(false)
+        end
+
+        it "disabled true if td.TREASURE_DATA_API_KEY missing" do
+          FileUtils.mkdir_p("#{TMP_DIR}/config")
+          File.unlink("#{TMP_DIR}/config/treasure_data.yml") rescue nil
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_DEPLOY, "w") {|f|
+            f.write <<EOF
+EOF
+          }
+          c = TreasureData::Logger::Agent::Rails::Config.init
+          expect(c.disabled).to eq(true)
+        end
+      end
+
+      context "config_path and CONFIG_PATH_EY_DEPLOY does not exists" do
+        it "load from CONFIG_PATH_EY_LOCAL" do
+          FileUtils.mkdir_p("#{TMP_DIR}/config")
+          File.unlink("#{TMP_DIR}/config/treasure_data.yml") rescue nil
+          File.unlink(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_DEPLOY) rescue nil
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_LOCAL, "w") {|f|
+            f.write <<EOF
+td:
+  TREASURE_DATA_API_KEY: foo
+EOF
+          }
+          c = TreasureData::Logger::Agent::Rails::Config.init
+          expect(c.disabled).to eq(false)
+        end
+
+        it "disabled true if td.TREASURE_DATA_API_KEY missing" do
+          FileUtils.mkdir_p("#{TMP_DIR}/config")
+          File.unlink("#{TMP_DIR}/config/treasure_data.yml") rescue nil
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_LOCAL, "w") {|f|
+            f.write <<EOF
+EOF
+          }
+          c = TreasureData::Logger::Agent::Rails::Config.init
+          expect(c.disabled).to eq(true)
+        end
+      end
+
+      context "config_path, CONFIG_PATH_EY_DEPLOY, CONFIG_PATH_EY_LOCAL are all exists" do
+        it "load from CONFIG_PATH_EY_LOCAL" do
+          FileUtils.mkdir_p("#{TMP_DIR}/config")
+          File.open("#{TMP_DIR}/config/treasure_data.yml", "w") {|f|
+            f.write <<EOF
+test:
+  agent: localhost
+  tag: foo.bar
+  test_mode: false
+EOF
+          }
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_DEPLOY, "w") {|f|
+            f.write <<EOF
+test:
+  agent: localhost
+  tag: foo.bar
+  test_mode: true
+EOF
+          }
+          File.open(TreasureData::Logger::Agent::Rails::CONFIG_PATH_EY_LOCAL, "w") {|f|
+            f.write <<EOF
+test:
+  agent: localhost
+  tag: foo.bar
+  test_mode: true
+EOF
+          }
+          c = TreasureData::Logger::Agent::Rails::Config.init
+          expect(c.test_mode).to eq(false)
+        end
+      end
     end
   end
 end
