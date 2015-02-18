33fa3bad0b13affe1b9cdf840f8ac3c2e2ce198f
uu59 <k@uu59.org>
2015-02-18 11:30:09 +0900

    Coveralls run on MRI only as commented reason

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
-require 'simplecov'
-require 'coveralls'
+if defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'ruby'
+  # SimpleCov officially supports MRI 1.9+ only for now
+  # https://github.com/colszowka/simplecov#ruby-version-compatibility
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start("test_frameworks")
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start("test_frameworks")
+end
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
